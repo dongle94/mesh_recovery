041dc85ead83e4bed0818325717d576872d34ad4
dongle94 <sdy1029@gmail.com>
2024-02-27 10:31:46 +0900

    Feat: Detection Task
    - Object Detection Task 추가
    - 현재는 디텍션 수행 후 박스들의 정보를 리스트 형태로 det_ret으로item에 담아 전송
      - hybrik의 전처리 테스크 진행 해보고 전처리를 디텍션 내부로 붙일지 결정 예정

--- core/hybrik/tasks.py
+++ core/hybrik/tasks.py
@@ -4,8 +4,7 @@ import sys
 ROOT_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(ROOT_PATH)
 
-from core.media_loader import MediaLoader
-from core.mp import mp_queue, task
+from core.mp import task
 
 
 class VideoInputTask(task.Job):
@@ -20,6 +19,7 @@ class VideoInputTask(task.Job):
         self.bgr = bgr
 
     def init(self):
+        from core.media_loader import MediaLoader
         self.media_loader = MediaLoader(self.source)
         self.f_count = 0
 
@@ -34,4 +34,40 @@ class VideoInputTask(task.Job):
             'end': stop
         }
         self.f_count += 1
+        return item
+
+
+class ObjectDetectionTask(task.Job):
+    def __init__(self, opt, i=0):
+        super().__init__(i=i)
+        self.opt = opt
+
+        self.detector = None
+        self.f_count = 0
+
+    def init(self):
+        from core.obj_detector import ObjectDetector
+        from utils.logger import init_logger
+
+        init_logger(self.opt)
+
+        self.detector = ObjectDetector(cfg=self.opt)
+        self.f_count = 0
+
+    def process(self, item=None):
+        if item is None or item['end'] is True:
+            return None
+
+        frame = item['image']
+
+        det = self.detector.run(frame)
+
+        det_ret = []
+        for d in det:
+            x1, y1, x2, y2 = map(int, d[:4])
+            # cv2.rectangle(frame, (x1, y1), (x2, y2), (96, 96, 216), thickness=2, lineType=cv2.LINE_AA)
+            det_ret.append([x1, y1, x2, y2])
+
+        item['det_ret'] = det_ret
+
         return item
