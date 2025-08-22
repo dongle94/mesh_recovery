f78936f2c95166e8c8a9501b79324b863c656fa0
dongle94 <sdy1029@gmail.com>
2025-08-22 19:03:57 +0900

    feat(constants): add JOINT_NAMES and JOINT_IDS for joint mapping

--- core/vibe/models/smpl.py
+++ core/vibe/models/smpl.py
@@ -10,53 +10,7 @@ from smplx.utils import ModelOutput, SMPLOutput
 from smplx.lbs import vertices2joints
 
 from utils.config import get_config
-
-# from lib.core.config import VIBE_DATA_DIR
-
-# Map joints to SMPL joints
-JOINT_MAP = {
-    'OP Nose': 24, 'OP Neck': 12, 'OP RShoulder': 17,
-    'OP RElbow': 19, 'OP RWrist': 21, 'OP LShoulder': 16,
-    'OP LElbow': 18, 'OP LWrist': 20, 'OP MidHip': 0,
-    'OP RHip': 2, 'OP RKnee': 5, 'OP RAnkle': 8,
-    'OP LHip': 1, 'OP LKnee': 4, 'OP LAnkle': 7,
-    'OP REye': 25, 'OP LEye': 26, 'OP REar': 27,
-    'OP LEar': 28, 'OP LBigToe': 29, 'OP LSmallToe': 30,
-    'OP LHeel': 31, 'OP RBigToe': 32, 'OP RSmallToe': 33, 'OP RHeel': 34,
-    'Right Ankle': 8, 'Right Knee': 5, 'Right Hip': 45,
-    'Left Hip': 46, 'Left Knee': 4, 'Left Ankle': 7,
-    'Right Wrist': 21, 'Right Elbow': 19, 'Right Shoulder': 17,
-    'Left Shoulder': 16, 'Left Elbow': 18, 'Left Wrist': 20,
-    'Neck (LSP)': 47, 'Top of Head (LSP)': 48,
-    'Pelvis (MPII)': 49, 'Thorax (MPII)': 50,
-    'Spine (H36M)': 51, 'Jaw (H36M)': 52,
-    'Head (H36M)': 53, 'Nose': 24, 'Left Eye': 26,
-    'Right Eye': 25, 'Left Ear': 28, 'Right Ear': 27
-}
-JOINT_NAMES = [
-    'OP Nose', 'OP Neck', 'OP RShoulder',
-    'OP RElbow', 'OP RWrist', 'OP LShoulder',
-    'OP LElbow', 'OP LWrist', 'OP MidHip',
-    'OP RHip', 'OP RKnee', 'OP RAnkle',
-    'OP LHip', 'OP LKnee', 'OP LAnkle',
-    'OP REye', 'OP LEye', 'OP REar',
-    'OP LEar', 'OP LBigToe', 'OP LSmallToe',
-    'OP LHeel', 'OP RBigToe', 'OP RSmallToe', 'OP RHeel',
-    'Right Ankle', 'Right Knee', 'Right Hip',
-    'Left Hip', 'Left Knee', 'Left Ankle',
-    'Right Wrist', 'Right Elbow', 'Right Shoulder',
-    'Left Shoulder', 'Left Elbow', 'Left Wrist',
-    'Neck (LSP)', 'Top of Head (LSP)',
-    'Pelvis (MPII)', 'Thorax (MPII)',
-    'Spine (H36M)', 'Jaw (H36M)',
-    'Head (H36M)', 'Nose', 'Left Eye',
-    'Right Eye', 'Left Ear', 'Right Ear'
-]
-
-JOINT_IDS = {JOINT_NAMES[i]: i for i in range(len(JOINT_NAMES))}
-# JOINT_REGRESSOR_TRAIN_EXTRA = osp.join(VIBE_DATA_DIR, 'J_regressor_extra.npy')
-# SMPL_MEAN_PARAMS = osp.join(VIBE_DATA_DIR, 'smpl_mean_params.npz')
-# SMPL_MODEL_DIR = VIBE_DATA_DIR
+from core.spin.constants import JOINT_NAMES, JOINT_MAP
 
 
 class SMPL(_SMPL):
