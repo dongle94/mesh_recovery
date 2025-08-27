7932bbfcac5bb4b892d3b742a7a7093e3f167485
dongle94 <sdy1029@gmail.com>
2025-08-27 13:33:53 +0900

    feat(vibe): integrate object tracking and VIBE processing for video input
    - 렌더링 이식 전까지 작업
    - 롤에 따른 메소드 분리 필요

--- core/vibe/vibe.py
+++ core/vibe/vibe.py
@@ -18,9 +18,11 @@ Contact: ps-license@tuebingen.mpg.de
 import os
 import sys
 import cv2
+import numpy as np
 import torch
 import torch.nn as nn
-import numpy as np
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 from typing import Dict, List, Optional, Tuple
 from pathlib import Path
 
@@ -31,6 +33,13 @@ if str(ROOT) not in sys.path:
 
 from utils.logger import get_logger
 from core.vibe.models.vibe import VIBE_Demo
+from core.vibe.dataset.inference import Inference
+from core.vibe.models.mpt import run_tracker, prepare_output_tracks
+from core.vibe.utils.demo_utils import (
+	video_to_images,
+    convert_crop_cam_to_orig_img,
+    convert_crop_coords_to_orig_img
+)
 
 
 class VIBE(nn.Module):
@@ -62,7 +71,7 @@ class VIBE(nn.Module):
         # placeholder: implement loading of VIBE backbone, temporal model, SMPL, etc.
         self.model = VIBE_Demo(
             seqlen=16,
-            batch_size=64,
+            batch_size=self.config.vibe_batch_size,
             n_layers=2,
             hidden_size=1024,
             add_linear=True,
@@ -125,10 +134,18 @@ class VIBE(nn.Module):
     def __repr__(self) -> str:
         return f"VIBE(device={self.device}, initialized={self.is_initialized})"
 
+    def process_detection(self, image_folder: str, detector):
+        # run tracker
+        trackers = run_tracker(image_folder, detector)
+        
+        tracking_results = prepare_output_tracks(trackers)
+
+        return tracking_results
 
 if __name__ == "__main__":
     from utils.logger import init_logger
     from utils.config import set_config, get_config
+    from core.obj_detector import ObjectDetector
 
     set_config('./configs/config.yaml')
     _cfg = get_config()
@@ -138,4 +155,102 @@ if __name__ == "__main__":
 
     OUTPUT_FOLDER = Path("out") / os.path.basename(_cfg.media_source)
 
+    _detector = ObjectDetector(cfg=_cfg)
     _VIBE = VIBE(_cfg, device=_cfg.device, logger=_logger)
+
+    # ========= Run tracking ========= #
+    bbox_scale = 1.1
+    image_folder, num_frames, img_shape = video_to_images(_cfg.media_source, return_info=True)
+    print(f'Input video number of frames {num_frames}')
+    orig_height, orig_width = img_shape[:2]
+    tracking_results = _VIBE.process_detection(image_folder, _detector)
+
+    vibe_results = {}
+    # ========= Run VIBE on each person ========= #
+    for person_id in tqdm(list(tracking_results.keys())):
+        bboxes = joints2d = None
+
+        bboxes = tracking_results[person_id]['bbox']
+        frames = tracking_results[person_id]['frames']
+
+        dataset = Inference(
+            image_folder=image_folder,
+            frames=frames,
+            bboxes=bboxes,
+            joints2d=joints2d,
+            scale=bbox_scale,
+        )
+
+        bboxes = dataset.bboxes
+        frames = dataset.frames
+
+        has_keypoints = True if joints2d is not None else False
+
+        dataloader = DataLoader(dataset, batch_size=_cfg.vibe_batch_size, num_workers=16)
+
+        with torch.no_grad():
+            pred_cam, pred_verts, pred_pose, pred_betas, pred_joints3d, smpl_joints2d, norm_joints2d = \
+                [], [], [], [], [], [], []
+            for batch in dataloader:
+                if has_keypoints:
+                    batch, nj2d = batch
+                    norm_joints2d.append(nj2d.numpy().reshape(-1, 21, 3))
+
+                batch = batch.unsqueeze(0)
+                batch = batch.to(_VIBE.device)
+
+                batch_size, seqlen = batch.shape[:2]
+                output = _VIBE.model(batch)[-1]
+
+                pred_cam.append(output['theta'][:, :, :3].reshape(batch_size * seqlen, -1))
+                pred_verts.append(output['verts'].reshape(batch_size * seqlen, -1, 3))
+                pred_pose.append(output['theta'][:,:,3:75].reshape(batch_size * seqlen, -1))
+                pred_betas.append(output['theta'][:, :,75:].reshape(batch_size * seqlen, -1))
+                pred_joints3d.append(output['kp_3d'].reshape(batch_size * seqlen, -1, 3))
+                smpl_joints2d.append(output['kp_2d'].reshape(batch_size * seqlen, -1, 2))
+            
+            pred_cam = torch.cat(pred_cam, dim=0)
+            pred_verts = torch.cat(pred_verts, dim=0)
+            pred_pose = torch.cat(pred_pose, dim=0)
+            pred_betas = torch.cat(pred_betas, dim=0)
+            pred_joints3d = torch.cat(pred_joints3d, dim=0)
+            smpl_joints2d = torch.cat(smpl_joints2d, dim=0)
+            del batch
+
+        # ========= Save results to a pickle file ========= #
+        pred_cam = pred_cam.cpu().numpy()
+        pred_verts = pred_verts.cpu().numpy()
+        pred_pose = pred_pose.cpu().numpy()
+        pred_betas = pred_betas.cpu().numpy()
+        pred_joints3d = pred_joints3d.cpu().numpy()
+        smpl_joints2d = smpl_joints2d.cpu().numpy()
+
+        print(pred_cam.shape, bboxes.shape)
+        orig_cam = convert_crop_cam_to_orig_img(
+            cam=pred_cam,
+            bbox=bboxes,
+            img_width=orig_width,
+            img_height=orig_height
+        )
+
+        joints2d_img_coord = convert_crop_coords_to_orig_img(
+            bbox=bboxes,
+            keypoints=smpl_joints2d,
+            crop_size=224,
+        )
+
+        output_dict = {
+            'pred_cam': pred_cam,
+            'orig_cam': orig_cam,
+            'verts': pred_verts,
+            'pose': pred_pose,
+            'betas': pred_betas,
+            'joints3d': pred_joints3d,
+            'joints2d': joints2d,
+            'joints2d_img_coord': joints2d_img_coord,
+            'bboxes': bboxes,
+            'frame_ids': frames,
+        }
+
+        vibe_results[person_id] = output_dict
+        print("완료")
